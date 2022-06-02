0e4488d66bb3bea8ba1692165817b103eb122286
Rakesh <rakesh@gmail.com>
2022-06-02 15:56:37 +0530

    Added Entity edit-1

---
+++ Laptop/src/main/java/com/laptop/entity/Laptop.java
@@ -0,0 +1,94 @@
+package com.laptop.entity;
+
+public class Laptop {
+
+	
+	private int id;
+	
+	private String name;
+	private String ramAvailable;
+	private String osName;
+	private String webcam;
+	private int price;
+	
+	public Laptop() {
+		super();
+	}
+
+	public Laptop(String name, String ramAvailable, String osName, String webcam, int price) {
+		super();
+		this.name = name;
+		this.ramAvailable = ramAvailable;
+		this.osName = osName;
+		this.webcam = webcam;
+		this.price = price;
+	}
+
+	public Laptop(int id, String name, String ramAvailable, String osName, String webcam, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.ramAvailable = ramAvailable;
+		this.osName = osName;
+		this.webcam = webcam;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRamAvailable() {
+		return ramAvailable;
+	}
+
+	public void setRamAvailable(String ramAvailable) {
+		this.ramAvailable = ramAvailable;
+	}
+
+	public String getOsName() {
+		return osName;
+	}
+
+	public void setOsName(String osName) {
+		this.osName = osName;
+	}
+
+	public String getWebcam() {
+		return webcam;
+	}
+
+	public void setWebcam(String webcam) {
+		this.webcam = webcam;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Laptop [id=" + id + ", name=" + name + ", ramAvailable=" + ramAvailable + ", osName=" + osName
+				+ ", webcam=" + webcam + ", price=" + price + "]";
+	}
+	
+	
+	
+	
+}
